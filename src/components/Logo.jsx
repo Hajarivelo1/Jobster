9311a8c2598a6dc7f34844ec7e01626e3cc85c36
Hajarivelo1 <93727504+Hajarivelo1@users.noreply.github.com>
2023-04-12 11:01:01 +0300

    jobster

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/images/logo.png'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <img src={logo} alt="jobify logo" className="logo" />
+  )
+}
+
+export default Logo
